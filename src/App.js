ad9d92848eccf33f273613925297d44fd780667d
HASURO <rentaro170928@icloud.com>
2023-07-02 15:46:18 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Home/>
+    </>
   );
 }
+
+export const resas = axios.create({
+  baseURL: 'https://opendata.resas-portal.go.jp/api/v1/',
+  headers: {
+      'Content-Type': 'application/json',
+      'X-API-KEY': 'TYYuFGHmMr2iPVizp07zoTJ9Qo0nHiR60ZB0DnA4',
+  },
+  responseType: 'json'
+});
 
 export default App;
